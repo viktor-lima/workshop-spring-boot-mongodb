7ee309714feb052dccff29d5e01283ad190a27b4
viktor-lima <joaolimak10@outlook.com>
2021-11-10 18:35:40 -0300

    Atualização de usuario com PUT

--- src/main/java/com/vkl/workshopmongo/services/UserService.java
+++ src/main/java/com/vkl/workshopmongo/services/UserService.java
@@ -25,18 +25,32 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-	
+
 	public void delete(String id) {
 		findById(id);
 		repo.deleteById(id);
 	}
-	
+
+	public User update(User obj) {
+		Optional<User> obj1 = repo.findById(obj.getId());
+		User newObj = obj1.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		updateData(newObj, obj);
+
+		return repo.save(newObj);
+
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
-	
+
 }
