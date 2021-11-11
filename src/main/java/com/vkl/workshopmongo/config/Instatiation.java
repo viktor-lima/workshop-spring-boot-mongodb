924c4ea9dd34078a7b5ce7055a027c94b62ee07a
viktor-lima <joaolimak10@outlook.com>
2021-11-11 18:24:07 -0300

    Acrecentando comentarios aos posts

--- src/main/java/com/vkl/workshopmongo/config/Instatiation.java
+++ src/main/java/com/vkl/workshopmongo/config/Instatiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.vkl.workshopmongo.domain.Post;
 import com.vkl.workshopmongo.domain.User;
 import com.vkl.workshopmongo.dto.AuthorDTO;
+import com.vkl.workshopmongo.dto.ComentDTO;
 import com.vkl.workshopmongo.repository.PostRepository;
 import com.vkl.workshopmongo.repository.UserRepository;
 
@@ -41,6 +42,13 @@ public class Instatiation implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("21/03/2018") , "Partiu viagen", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018") , "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
 
+		ComentDTO c1 = new ComentDTO("Boa viagen mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		ComentDTO c2 = new ComentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		ComentDTO c3 = new ComentDTO("Temha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		
+		post1.getComents().addAll(Arrays.asList(c1,c2));
+		post2.getComents().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1,post2));
 		
--- src/main/java/com/vkl/workshopmongo/domain/Post.java
+++ src/main/java/com/vkl/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.vkl.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.vkl.workshopmongo.dto.AuthorDTO;
+import com.vkl.workshopmongo.dto.ComentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -19,6 +22,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<ComentDTO> coments = new ArrayList<>();
 
 	public Post() {
 	}
@@ -70,6 +75,16 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	
+	
+	public List<ComentDTO> getComents() {
+		return coments;
+	}
+
+	public void setComents(List<ComentDTO> coments) {
+		this.coments = coments;
 	}
 
 	@Override
